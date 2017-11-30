48fcbce7dc4b615562725453d31d1d1d43a129ba
Jonathan Zhang <zhangjianan1996@qq.com>
2017-11-30 23:27:52 +0800

    update bossinfo

--- app/src/component/avatar-selector/avatar-selector.js
+++ app/src/component/avatar-selector/avatar-selector.js
@@ -8,11 +8,21 @@ class AvatarSelector extends React.Component {
       elem: ''
     }
   }
+  componentWillMount(){
+    console.log("渲染开始");
+  }
+  componentDidMount(){
+    console.log("渲染完毕");
+  }
   render() {
-    const avatorList = 'man,woman,boy,girl,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'.split(',').map(v => ({
-      icon: require(`../img/${v}.png`),
-      text: v
+    setTimeout(function(){
+			window.dispatchEvent(new Event('resize'))
+		},0)
+    const avatorList = 'man,woman,boy,girl,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'.split(',').map((val)=> ({
+      icon: require(`../img/${val}.png`),
+      text: val
     }))
+    console.log(avatorList);
     const gridHeader = this.state.text
       ? (<div>
         <span style={{fontSize:18}}>已选择头像: </span>
@@ -21,7 +31,7 @@ class AvatarSelector extends React.Component {
       : <div style={{fontSize:18}}>请选择头像</div>
     return (<div>
       <List renderHeader={() => gridHeader}>
-        <Grid data={avatorList} columnNum={5} onClick={elem => {
+        <Grid data={avatorList} isCarousel={true} itemStyle={{verticalAlign: 'top' }} columnNum={4} onClick={elem => {
             this.setState(elem)
             this.props.selectAvatar(elem.text)
           }}/>
